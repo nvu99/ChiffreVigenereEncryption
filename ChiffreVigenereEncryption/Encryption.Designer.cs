5d4c22a24bcbdfae100fadfe371ca85642f49b97
nvu99 <thaidaivu@gmail.com>
2014-11-09 20:03:59 +0100

    work in progress

---
+++ ChiffreVigenereEncryption/Encryption.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ChiffreVigenereEncryption
+{
+    partial class Encryption
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.inputBox = new System.Windows.Forms.RichTextBox();
+            this.outputBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(224, 13);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(938, 31);
+            this.keyBox.TabIndex = 0;
+            this.keyBox.TextChanged += new System.EventHandler(this.keyBox_TextChanged);
+            // 
+            // inputBox
+            // 
+            this.inputBox.BackColor = System.Drawing.SystemColors.InfoText;
+            this.inputBox.ForeColor = System.Drawing.Color.LawnGreen;
+            this.inputBox.Location = new System.Drawing.Point(224, 59);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(938, 325);
+            this.inputBox.TabIndex = 1;
+            this.inputBox.Text = "";
+            this.inputBox.TextChanged += new System.EventHandler(this.inputBox_TextChanged);
+            // 
+            // outputBox
+            // 
+            this.outputBox.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.outputBox.ForeColor = System.Drawing.Color.Red;
+            this.outputBox.Location = new System.Drawing.Point(224, 404);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(938, 325);
+            this.outputBox.TabIndex = 2;
+            this.outputBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Input";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 407);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Output";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(17, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(194, 175);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Bug Caution! If you\r\nhave text in your\r\ninput do not delete\r\nthe key! This will \r" +
+                "\ncause the\r\napplication\r\nto crash!";
+            // 
+            // Encryption
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1174, 738);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.keyBox);
+            this.Name = "Encryption";
+            this.Text = "Chiffre Vigenere Encryption";
+            this.Load += new System.EventHandler(this.Encryption_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox keyBox;
+        private System.Windows.Forms.RichTextBox inputBox;
+        private System.Windows.Forms.RichTextBox outputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ChiffreVigenereEncryption/Encryption.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChiffreVigenereEncryption
+{
+    public partial class Encryption : Form
+    {
+
+        string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789 \\/~`!@#$%^&*(),._+{}[]\"-=;'\n<>|";
+        int indexInLoop = 1;
+        string inputInLoop;
+        string outputInLoop;
+        string output;
+        int shiftrate;
+        int keyLength;
+        int keyIndex;
+        string keyInLoop;
+
+        public Encryption()
+        {
+            InitializeComponent();
+            inputBox.Enabled = false;
+            outputBox.ReadOnly = true;
+            
+        }
+
+        private void inputBox_TextChanged(object sender, EventArgs e)
+        {
+            keyLength = keyBox.Text.Length;
+            int numberOfCharacters = inputBox.Text.Length;
+            indexInLoop = 1;
+            int i1 = 0;
+            output = "";
+            string key = keyBox.Text;
+            keyIndex = 0;
+
+            while (indexInLoop <= numberOfCharacters) 
+            {
+                keyIndex++;
+                if (keyIndex > keyLength)
+                {
+                    keyIndex = 1;
+                    keyInLoop = keyBox.Text.Substring(keyIndex - 1, 1);
+                    shiftrate = alphabet.IndexOf(keyInLoop);
+                }
+
+                if (keyLength == 0)
+                {
+                    shiftrate = 0;
+                }
+
+                else
+                {
+                    keyInLoop = keyBox.Text.Substring(keyIndex - 1, 1);
+                    shiftrate = alphabet.IndexOf(keyInLoop);
+                }
+
+                inputInLoop = inputBox.Text.Substring(indexInLoop - 1, 1);
+                int pos = alphabet.IndexOf(inputInLoop);
+                if (pos + shiftrate <= 92)
+                {
+                    outputInLoop = alphabet.Substring(pos + shiftrate, 1);
+                    output += outputInLoop;
+                    indexInLoop++;
+                    outputInLoop = "";
+                    outputBox.Text = output;
+                }
+                else
+                {
+                    i1 = pos - 92 + shiftrate - 1;
+                    outputInLoop = alphabet.Substring(i1, 1);
+                    output += outputInLoop;
+                    indexInLoop++;
+                    outputInLoop = "";
+                    outputBox.Text = output;
+                }
+
+                }
+                if (inputBox.Text.Length == 0)
+                {
+                    outputBox.Text = "";
+                }
+            
+        }
+
+        private void keyBox_TextChanged(object sender, EventArgs e)
+        {
+            keyLength = keyBox.Text.Length;
+            int numberOfCharacters = inputBox.Text.Length;
+            indexInLoop = 1;
+            int i1 = 0;
+            output = "";
+            string key = keyBox.Text;
+            keyIndex = 0;
+
+            if (keyLength == 0)
+            {
+                inputBox.Enabled = false;
+                inputBox.Text = "";
+            }
+            else
+            {
+                inputBox.Enabled = true;
+            }
+
+            while (indexInLoop <= numberOfCharacters)
+            {
+                keyIndex++;
+                if (keyIndex > keyLength)
+                {
+                    keyIndex = 1;
+                    keyInLoop = keyBox.Text.Substring(keyIndex - 1, 1);
+                    shiftrate = alphabet.IndexOf(keyInLoop);
+                }
+
+                if (keyLength == 0)
+                {
+                    shiftrate = 0;
+                }
+
+                else
+                {
+                    keyInLoop = keyBox.Text.Substring(keyIndex - 1, 1);
+                    shiftrate = alphabet.IndexOf(keyInLoop);
+                }
+
+                inputInLoop = inputBox.Text.Substring(indexInLoop - 1, 1);
+                int pos = alphabet.IndexOf(inputInLoop);
+                if (pos + shiftrate <= 92)
+                {
+                    outputInLoop = alphabet.Substring(pos + shiftrate, 1);
+                    output += outputInLoop;
+                    indexInLoop++;
+                    outputInLoop = "";
+                    outputBox.Text = output;
+                }
+                else
+                {
+                    i1 = pos - 92 + shiftrate - 1;
+                    outputInLoop = alphabet.Substring(i1, 1);
+                    output += outputInLoop;
+                    indexInLoop++;
+                    outputInLoop = "";
+                    outputBox.Text = output;
+                }
+
+            }
+            if (inputBox.Text.Length == 0)
+            {
+                outputBox.Text = "";
+            }
+        }
+
+        private void Encryption_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
